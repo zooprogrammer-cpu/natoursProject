f5fc58c0954d57a41d27bbb6d26274df7d54cfdd
AshB <ashbasn@gmaill.com>
2024-02-10 15:36:21 -0600

    Creating our own middleware

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ const app = express();
 // middleware to put body data in the request
 // it sits between the request and the response
 app.use(express.json());
+// We use app.use to use middleware.
+// calling express.json() returns a function that gets added
+// to the middleware stack.
+
+// Creating our own middleware function
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+})
+
+// another middleware to manipulate the request object -
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status : 'success',
+    requestedAt : req.requestTime,
     results : tours.length,
     data : {
       tours: tours
